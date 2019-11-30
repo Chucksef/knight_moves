85885c84c4814884c943d53c6408bef0654daae2
Chucksef <Chucksef@gmail.com>
2019-11-30 16:08:04 -0700

    finished board setup method and class

--- lib/game.rb
+++ lib/game.rb
@@ -9,10 +9,14 @@ class Game
 
     def set_up_board
         spaces = []
-        8.times do
+        spaces << ["R", "N", "B", "Q", "K", "B", "N", "R"]
+        spaces << ["P", "P", "P", "P", "P", "P", "P", "P"]
+        4.times do
             spaces << Array.new(8) {" "}
         end
-        spaces.each_with_index { |row, idx| puts "row #{idx}: #{row}"}
+        spaces << ["p", "p", "p", "p", "p", "p", "p", "p"]
+        spaces << ["r", "n", "b", "q", "k", "b", "n", "r"]
+        spaces
     end
 
     def print_board
@@ -23,21 +27,20 @@ class Game
         puts ""
         8.times {head += " _______"}
         puts head
+        row = 0
         8.times do
             lines = Array.new(4) {"|"}
+            col = 0
             8.times do
                 lines[0] += "       |"
                 lines[1] += "       |"
-                lines[2] += "       |"
+                lines[2] += "   #{@pieces[row][col]}   |"
                 lines[3] += "_______|"
+                col += 1
             end
             lines.each {|l| puts l}
+            row += 1
         end
         4.times {puts ""}
     end
-
-
 end
-
-game = Game.new
-game.print_board
---
+++ main.rb
@@ -0,0 +1,5 @@
+require "./lib/game"
+require "./lib/knight"
+
+game = Game.new
+game.print_board
